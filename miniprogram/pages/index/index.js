1b7ee56099986ce8cb5010f936a4297c853eaceb
elliotshi <roxylee@shixiaoyudeMacBook-Air.local>
2019-07-30 18:43:16 +0800

    first commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,29 @@
+// miniprogram/pages/index/index.js
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    }
+  },
+  bindGetUserInfo: function (e) {
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    }, () => {
+      wx.switchTab({
+        url: e.currentTarget.dataset.path,
+      })
+    })
+  }
+})
